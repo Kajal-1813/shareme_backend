6adbd74b300dad054ac7e5c53364b586d893b118
kajal.singh <kajalsingh1813@gmail.com>
2023-11-13 22:38:43 +0530

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'kajal-shareme',
+
+  projectId: '7ftj2kzf',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
